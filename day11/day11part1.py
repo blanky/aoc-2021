320d67118cfc6fbb60bc421b88157630f8f2fbf9
blanky <noo@oo.oo>
2021-12-11 18:48:03 +1000

    day11

---
+++ day11/day11part1.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+# with open("test.txt", 'r') as f:
+with open("input.txt", 'r') as f:
+    map = []
+    for line in f:
+        line = line.replace('\n', '')
+        map.append([int(i) for i in line])
+
+
+def flash(x, y, map):
+    surrounding_points = []
+    for dx in range(-1, 2, 1):
+        for dy in range(-1, 2, 1):
+            surrounding_points.append((x+dx, y+dy))
+    surrounding_points.remove((x,y))
+    max_x = len(map[0])
+    max_y = len(map)
+    for point in surrounding_points:
+        if (not (point[0] < 0 or point[1] < 0 or point[0] >= max_x or point[1] >= max_y)) and \
+           (map[point[1]][point[0]] != 0):
+            map[point[1]][point[0]] += 1
+    return map
+        
+def new_flashes(map):
+    for y in range(len(map)):
+        for x in range(len(map)):
+            if(map[y][x] > 9):
+                return True
+
+def background_gain(map):
+    for y in range(len(map)):
+        for x in range(len(map)):
+            map[y][x] += 1
+    return map
+
+def print_map(map):
+    for line in map:
+        line_str = ''
+        for i in line:
+            line_str += str(i)
+        print(line_str)
+
+
+print("Before steps")
+print_map(map)
+flashes = 0    
+for step in range(1, 101):
+    map = background_gain(map)
+    while new_flashes(map):
+        for y in range(len(map)):
+            for x in range(len(map)):
+                if(map[y][x] > 9):
+                    flashes += 1
+                    map = flash(x, y, map)
+                    map[y][x] = 0
+    print("Step %i" % (step))
+    print_map(map)
+    
+print("Flashes: %i" % (flashes))
